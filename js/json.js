14ceaff59bf7ddcd9c66deb2a5ab765e8012982b
jamaldine <jboukir@gmail.com>
2021-02-03 17:21:19 +0100

    right home in prog

--- js/json.js
+++ js/json.js
@@ -1,45 +1,47 @@
 $(document).ready(function() {
     $(function() {
         $.getJSON('../data.json', function(data) {
-            const schema1 = '<section class="home-skills">'+
-            '<h2 class="skill-title">'+data.webIntegrator.name+'</h2>'+
-                '<div class="skills-list"></div>'+
-                '<div class="single-item"></div>'+
+            const schema1 = '<section class="home-skills">' +
+                '<h2 class="skill-title">' + data.webIntegrator.name + '</h2>' +
             '</section>';
             $(schema1).appendTo(".home-right");
-        });
-        $.getJSON('../data.json', function(data) {
-            $.each(data.webIntegrator.skills, function(i, item) {
-                const schema2 = 
-                        '<div class="circle">'+
-                            '<img class="circle-img" src='+item.img+' />'+
-                        '</div>'+
-                        '<div class="skill">'+
-                            '<div class="name-percent">'+
-                                '<div class="name">'+item.name+'</div>'+
-                                '<div class="percent">'+item.percent+'</div>'+
-                            '</div>'+
-                            '<ul class="breadcrumb-'+i+'">'+
-                            
-                            '</ul>'+
+            $.each(data.webIntegrator.skills, function(k, skill) {
+                if (skill.type === "groupe-skills") {
+                    $.each(skill.list, function(i, item) {
+                        const l = k + i;
+                        const schema5 = `<div class='skills-list skills-list-${k+i}'></div>`;
+                        $(schema5).appendTo('.home-skills');
+                        const schema2 = '<div class="circle-skill">' +
+                            '<div class="circle">' +
+                                '<i class="' + item.img + '"></i>'+
+                            '</div>' +
+                            '<div class="skill">' +
+                                '<div class="name-percent">' +
+                                    '<div class="name">' + item.name + '</div>' +
+                                    '<div class="percent">' + item.percent + '</div>' +
+                                '</div>' +
+                                '<ul class="breadcrumb breadcrumb-' + l + '">' +
+                                '</ul>' +
+                            '</div>' +
                         '</div>';
-                $(schema2).appendTo(".skills-list");
-            });
-        });
+                        $(schema2).appendTo(`.skills-list-${k+i}`);
 
-        $.getJSON('../data.json', function(data) {
-            $.each(data.webIntegrator.skills, function(i, items) {
-                $.each(items.skillsItem, function(i, item) {
-                    const schema3 = '<li class="breadcrumb-item"><a class="breadcrumb-name"" href="#">'+item.name+'</a></li>';
-                    $(schema3).appendTo(`.breadcrumb-${i}`);
-                });
-            });
-        });
+                        $.each(item.skillsItem, function(j,skillItem) {
+                            console.log(skillItem, i + k);
+                            const schema3 = '<li class="breadcrumb-item"><a class="breadcrumb-name"" href="#">'+skillItem.name+'</a></li>';
+                            $(schema3).appendTo(`.breadcrumb-${i + k}`);
+                        });/**/
 
-       $.getJSON('../data.json', function(data) {
-            $.each(data.webIntegrator.singleSkills, function(i, item) {
-                const schema4 = '<div class="single-name" >' + item.name + '</div>';
-                $(schema4).appendTo(".single-item");
+                    });
+                }
+                /*else if (skill.type === 'single-skills') {
+                    $.each(skill.list, function(j, item) {
+                        const schema6 = `<div class="single-item single-item-${j}"></div>`;
+                        $(schema6).appendTo('.home-skills');
+                        const schema4 = '<div class="single-name">' + item.name + '</div>';
+                        $(schema4).appendTo(`.single-item-${j}`);
+                    });
+                }*/
             });
         });
     });
